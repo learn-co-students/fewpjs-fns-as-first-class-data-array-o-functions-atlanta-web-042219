ff0d4017bedee1f9fa15986d1842622fce12a2ce
github username <github email address>
2019-06-07 00:31:54 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function wakeDog(a, b) {
+  return `Wake ${a} the ${b}`;
+}
+
+function leashDog(a, b) {
+  return `Leash ${a} the ${b}`;
+}
+
+function walkToPark(a, b) {
+  return `Walk to the park with ${a} the ${b}`
+}
+
+function throwFrisbee(a, b) {
+  return `Throw the frisbee for ${a} the ${b}`
+}
+
+function walkHome(a, b) {
+  return `Walk home with ${a} the ${b}`
+}
+
+function unleashDog(a, b) {
+  return `Unleash ${a} the ${b}`
+}
+
+const routine = [
+  wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog
+]
+
+function exerciseDog(dogName, dogBreed) {
+  const tasks = [];
+  for (let task of routine) {
+    tasks.unshift(task(dogName, dogBreed));
+  }
+  return tasks;
+} 
